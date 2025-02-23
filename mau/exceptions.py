87d95c8751d83a352b4160c1c240fef73e5d08ee
Milinuri Nirvalen <pentergust@mail.ru>
2025-02-23 19:53:13 +0400

    Mau: Remove unused exceptions

--- mau/exceptions.py
+++ mau/exceptions.py
@@ -3,8 +3,6 @@
 Собраны в одном месте для удобства.
 """
 
-# TODO: Сделать исключение более конкретными
-
 
 class NoGameInChatError(Exception):
     """If there is no active game created in the chat.
@@ -35,23 +33,5 @@ class NotEnoughPlayersError(Exception):
 
 class DeckEmptyError(Exception):
     """When the deck runs out of cards."""
-
-    pass
-
-
-class ClassCoverError(Exception):
-    """When the user tries to cover with the wrong card."""
-
-    pass
-
-
-class GameNotStartedError(Exception):
-    """When the user tries to action in not started game."""
-
-    pass
-
-
-class NotGameOwnerError(Exception):
-    """When user tries to user room owner action."""
 
     pass
