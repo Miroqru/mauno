0ebfb43a7534e8c02c63e15d2ab1ac6853b0e334
Milinuri Nirvalen <pentergust@mail.ru>
2025-02-26 10:24:10 +0400

    Web: Add GameCard to HomePage

--- mau-web/src/pages/HomePage.vue
+++ mau-web/src/pages/HomePage.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
+import type { GameContext } from '@/share/api/types'
+import type { Ref } from 'vue'
 import NewGame from '@/components/buttons/NewGame.vue'
 import RandomGame from '@/components/buttons/RandomGame.vue'
 import ChallengeList from '@/components/home/ChallengeList.vue'
 import LeaderBoard from '@/components/home/LeaderBoard.vue'
-import RoomList from '@/components/home/RoomList.vue'
-import UserCard from '@/components/user/UserCard.vue'
 
-import { getRatingIndex } from '@/share/api'
+import RoomList from '@/components/home/RoomList.vue'
+import GameCard from '@/components/room/GameCard.vue'
+import UserCard from '@/components/user/UserCard.vue'
+import { getGame, getRatingIndex } from '@/share/api'
 import { useUserStore } from '@/share/stores/user'
-import { onMounted, ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 import RoomCard from '../components/room/RoomCard.vue'
 import UserCardPlaceholder from '../components/user/UserCardPlaceholder.vue'
 
 const userStore = useUserStore()
 const me = userStore.getMe()
 const room = userStore.fetchRoom()
+const game: Ref<GameContext | null> = ref(null)
 const isMobile = /android|iPad|iPhone|iPod/.test(navigator.userAgent)
 const topIndex = ref(0)
 
-onMounted(async () => {
-  if (me.value == null) {
+watchEffect(async () => {
+  if (me.value === null) {
     return
   }
   topIndex.value = await getRatingIndex(me.value.username, 'gems')
+  game.value = await getGame()
 })
 </script>
 
@@ -30,7 +35,8 @@ onMounted(async () => {
   <UserCard v-if="me" :user="me" :top-index="topIndex" />
   <UserCardPlaceholder v-else />
 
-  <RoomCard v-if="room && isMobile" :room="room" />
+  <GameCard v-if="game" :game="game" />
+  <RoomCard v-else-if="room && isMobile" :room="room" />
 
   <div class="md:flex md:gap-2">
     <section v-if="!isMobile" class="p-2 m-2 flex flex-col gap-2">
