391ce2336f90d8569ae8926ccd572954aaf951e5
Milinuri Nirvalen <pentergust@mail.ru>
2025-05-09 21:24:13 +0400

    Bot: Implement FastAPI+uvicorn webhook

--- maubot/bot.py
+++ maubot/bot.py
@@ -4,12 +4,15 @@
 """
 
 import asyncio
+import secrets
 import sys
-from collections.abc import Awaitable, Callable
-from typing import Any
+from collections.abc import AsyncGenerator, Awaitable, Callable
+from contextlib import asynccontextmanager
+from typing import Annotated, Any
 
 import uvicorn
 from aiogram import Bot, Dispatcher
+from aiogram.methods import TelegramMethod
 from aiogram.types import (
     CallbackQuery,
     ChosenInlineResult,
@@ -19,11 +22,8 @@ from aiogram.types import (
     Update,
 )
 from aiogram.utils.token import TokenValidationError
-from aiogram.webhook.aiohttp_server import (
-    SimpleRequestHandler,
-    setup_application,
-)
-from aiohttp import web
+from fastapi import APIRouter, FastAPI, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from maubot.config import config, default, sm
@@ -31,9 +31,6 @@ from maubot.context import get_context
 from maubot.events.journal import MessageJournal
 from maubot.events.router import er
 from maubot.handlers import ROUTERS
-
-# Константы
-# =========
 
 dp = Dispatcher(sm=sm)
 
@@ -97,6 +94,10 @@ async def catch_errors(event: ErrorEvent) -> None:
     )
 
 
+# Настройка webhook
+# =================
+
+
 async def on_startup(bot: Bot) -> None:
     """Настройка webhook при запуске."""
     logger.info("Set hook to: {}", config.hook_url)
@@ -105,16 +106,104 @@ async def on_startup(bot: Bot) -> None:
     )
 
 
+async def on_shutdown(bot: Bot) -> None:
+    """Настройка webhook при запуске."""
+    logger.info("Stop bot")
+    await bot.session.close()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    """Жизненный цикл бота."""
+    workflow_data = {
+        "app": app,
+        "dispatcher": dp,
+        **dp.workflow_data,
+    }
+
+    # db connected
+    await dp.emit_startup(**workflow_data)
+
+    yield
+
+    # app teardown
+    await dp.emit_shutdown(**workflow_data)
+
+
+router = APIRouter()
+
+
+@router.post(config.hook_root)
+async def process_hook(
+    update: Update,
+    secret: Annotated[
+        str, Header(alias="x-telegram-bot-api-secret-token")
+    ] = "",
+) -> dict:
+    """Обрабатывает приходящие события для бота."""
+    if not secrets.compare_digest(secret, config.hook_secret):
+        raise HTTPException(401, "Unauthorized")
+
+    result: TelegramMethod[Any] | None = await dp.feed_webhook_update(
+        dp.workflow_data["bot"], update
+    )
+
+    if not result:
+        return {}
+
+    logger.debug(result)
+    return {}
+
+
+#     writer = MultipartWriter(
+#         "form-data", boundary=f"webhookBoundary{secrets.token_urlsafe(16)}"
+#     )
+
+#     payload = writer.append(result.__api_method__)
+#     payload.set_content_disposition("form-data", name="method")
+
+#     files: dict[str, InputFile] = {}
+#     for key, value in result.model_dump(warnings=False).items():
+#         value = bot.session.prepare_value(value, bot=bot, files=files)
+#         if not value:
+#             continue
+#         payload = writer.append(value)
+#         payload.set_content_disposition("form-data", name=key)
+
+#     for key, value in files.items():
+#         payload = writer.append(value.read(bot))
+#         payload.set_content_disposition(
+#             "form-data",
+#             name=key,
+#             filename=value.filename or key,
+#         )
+
+#     return StreamingResponse(writer, media_type="multipart/form-data")
+
+
 # Запуск бота
 # ===========
 
 
-def create_app(bot: Bot) -> web.Application:
+def create_app(router: APIRouter) -> FastAPI:
     """Запускает работу Webhook."""
-    app = web.Application()
-    rh = SimpleRequestHandler(dp, bot, secret_token=config.hook_secret)
-    setup_application(app, dp, bot=bot)
-    rh.register(app, path=config.hook_root)
+    app: FastAPI = FastAPI(
+        debug=True,
+        title="Mau:bot",
+        summary="Play Mau with your friends in telegram chats",
+        version="2.2",
+        lifespan=lifespan,
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["https://localhost"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router)
     return app
 
 
@@ -136,17 +225,19 @@ def main() -> None:
         sys.exit("Check your bot token in .env file.")
 
     logger.info("Load handlers ...")
-    for router in ROUTERS:
-        dp.include_router(router)
-        logger.debug("Include router {}", router.name)
+    for r in ROUTERS:
+        dp.include_router(r)
+        logger.debug("Include router {}", r.name)
 
     logger.info("Set event handler")
     sm.set_handler(MessageJournal(bot, er))
 
     logger.success("Start polling!")
     if config.use_hook:
+        dp.workflow_data["bot"] = bot
         dp.startup.register(on_startup)
-        app = create_app(bot)
+        dp.shutdown.register(on_shutdown)
+        app = create_app(router)
         uvicorn.run(app, host=config.server_host, port=config.server_port)
     else:
         asyncio.run(dp.start_polling(bot))
