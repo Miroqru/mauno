34f58c0d4222cbbe2b79a730dfa7b186e5fb7fef
Milinuri Nirvalen <pentergust@mail.ru>
2025-10-11 15:23:12 +0400

    Deck: Remove standart presets

--- mau/deck/presets.py
+++ mau/deck/presets.py
@@ -6,9 +6,8 @@
 
 from collections.abc import Iterable, Iterator
 from dataclasses import dataclass
-from typing import Self
 
-from mau.deck import behavior
+from mau.deck.behavior import CardBehavior
 from mau.deck.card import MauCard
 from mau.deck.deck import Deck
 from mau.enums import CardColor
@@ -24,7 +23,7 @@ class CardGroup:
     К примеру: Числовые карты всех цветов по две на каждый цвет.
     """
 
-    behavior: behavior.NumberBehavior
+    behavior: CardBehavior
     value: int
     colors: Iterable[CardColor]
     count: int
@@ -36,12 +35,7 @@ class CardGroup:
         """
         for _ in range(self.count):
             for color in self.colors:
-                yield MauCard(
-                    color,
-                    self.value,
-                    self.behavior.cost or self.value,
-                    self.behavior,
-                )
+                yield MauCard(color, self.value, self.value, self.behavior)
 
 
 @dataclass(slots=True, frozen=True)
@@ -55,102 +49,6 @@ class DeckPreset:
     name: str
     desc: str
     groups: Iterable[CardGroup]
-
-
-# Пресеты колод
-# =============
-
-ALL_COLORS = (CardColor.RED, CardColor.YELLOW, CardColor.GREEN, CardColor.CYAN)
-
-_NUMBER = behavior.NumberBehavior()
-_TWIST = behavior.TwistBehavior()
-_ROTATE = behavior.RotateBehavior()
-_REVERSE = behavior.ReverseBehavior()
-_TURN = behavior.TurnBehavior()
-_TAKE = behavior.TakeBehavior()
-_TAKE_FOUR = behavior.WildTakeBehavior()
-_COLOR = behavior.WildColorBehavior()
-
-# Все готовые пресеты колод
-# TODO: Хранить в каких-нибудь настройках
-CARD_PRESETS: dict[str, DeckPreset] = {
-    "classic": DeckPreset(
-        name="🎻 Классика",
-        desc="Стандартная колода Уно",
-        groups=(
-            CardGroup(_ROTATE, 0, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 1, ALL_COLORS, 2),
-            CardGroup(_TWIST, 2, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 3, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 4, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 5, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 6, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 7, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 8, ALL_COLORS, 2),
-            CardGroup(_NUMBER, 9, ALL_COLORS, 2),
-            CardGroup(_REVERSE, 1, ALL_COLORS, 2),
-            CardGroup(_TURN, 1, ALL_COLORS, 2),
-            CardGroup(_TAKE, 2, ALL_COLORS, 2),
-            CardGroup(_TAKE_FOUR, 4, [CardColor.BLACK], 4),
-            CardGroup(_COLOR, 0, [CardColor.BLACK], 4),
-        ),
-    ),
-    "wild": DeckPreset(
-        name="🐍 Дикие карты",
-        desc="Меньше цифр, больше действий.",
-        groups=(
-            CardGroup(_ROTATE, 0, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 1, ALL_COLORS, 4),
-            CardGroup(_TWIST, 2, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 3, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 4, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 5, ALL_COLORS, 4),
-            CardGroup(_REVERSE, 1, ALL_COLORS, 4),
-            CardGroup(_TURN, 1, ALL_COLORS, 4),
-            CardGroup(_TAKE, 2, ALL_COLORS, 4),
-            CardGroup(_TAKE_FOUR, 4, [CardColor.BLACK], 6),
-            CardGroup(_COLOR, 0, [CardColor.BLACK], 6),
-        ),
-    ),
-    "casino": DeckPreset(
-        name="🎰 Казино",
-        desc="Нет слов, одни эмоции.",
-        groups=(
-            CardGroup(_ROTATE, 0, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 1, ALL_COLORS, 4),
-            CardGroup(_TWIST, 2, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 3, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 4, ALL_COLORS, 4),
-            CardGroup(_NUMBER, 5, ALL_COLORS, 4),
-            CardGroup(_REVERSE, 1, ALL_COLORS, 6),
-            CardGroup(_TURN, 1, ALL_COLORS, 6),
-            CardGroup(_TAKE, 2, ALL_COLORS, 6),
-            CardGroup(_TAKE_FOUR, 4, [CardColor.BLACK], 8),
-            CardGroup(_COLOR, 0, [CardColor.BLACK], 8),
-        ),
-    ),
-    "single": DeckPreset(
-        name="🗑️ Отладочные",
-        desc="По одной карте для каждого типа.",
-        groups=(
-            CardGroup(_ROTATE, 0, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 1, ALL_COLORS, 1),
-            CardGroup(_TWIST, 2, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 3, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 4, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 5, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 6, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 7, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 8, ALL_COLORS, 1),
-            CardGroup(_NUMBER, 9, ALL_COLORS, 1),
-            CardGroup(_REVERSE, 1, ALL_COLORS, 1),
-            CardGroup(_TURN, 1, ALL_COLORS, 1),
-            CardGroup(_TAKE, 2, ALL_COLORS, 1),
-            CardGroup(_TAKE_FOUR, 4, [CardColor.BLACK], 1),
-            CardGroup(_COLOR, 0, [CardColor.BLACK], 1),
-        ),
-    ),
-}
 
 
 class DeckGenerator:
@@ -177,8 +75,3 @@ class DeckGenerator:
     def deck(self) -> Deck:
         """Собирает новую колоду из правил."""
         return Deck(list(self._cards()))
-
-    @classmethod
-    def from_preset(cls, preset_name: str) -> Self:
-        """Получает новый генератор колоды по названию шаблона."""
-        return cls(list(CARD_PRESETS[preset_name].groups), preset_name)
