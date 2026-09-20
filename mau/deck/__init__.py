3cb3e2ddcbf624233a95db84130f1ba67cb0a9c3
Milinuri Nirvalen <pentergust@mail.ru>
2026-09-20 20:03:02 +0400

    Add public api for components

--- mau/deck/__init__.py
+++ mau/deck/__init__.py
@@ -9,3 +9,14 @@
 - Коллекцию колоды карт.
 - Заготовленные шаблоны для генерации колоды.
 """
+
+# Пере использование часто используемых компонентов
+from mau.deck.behavior import CardBehavior as CardBehavior
+
+# Реализация карты
+from mau.deck.card import CardColor as CardColor
+from mau.deck.card import MauCard as MauCard
+
+# Реализация колоды с картами
+from mau.deck.deck import Deck as Deck
+from mau.deck.deck import RandomDeck as RandomDeck
--- mau/game/__init__.py
+++ mau/game/__init__.py
@@ -8,3 +8,14 @@
 - Битовые игровые правила.
 - Реализация игрового револьвера.
 """
+
+from mau.game.game import MauGame as MauGame
+from mau.game.player import Player as Player
+from mau.game.player import PlayerID as PlayerID
+from mau.game.player import SortedCards as SortedCards
+from mau.game.player_manager import GameResult as GameResult
+from mau.game.player_manager import GameReverse as GameReverse
+from mau.game.player_manager import PlayerManager as PlayerManager
+from mau.game.player_manager import ResultType as ResultType
+from mau.game.settings import GameSettings as GameSettings
+from mau.game.shotgun import Shotgun as Shotgun
